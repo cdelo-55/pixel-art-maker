1ed461341b378e3268379fe31f1665964b9e87d8
Oberhauser <34341@nelnet.net>
2021-06-01 16:35:12 -0500

    day 1 pixel

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('click',function(){
+    let colorchoice = document.children[0].children[1].children[1].children[0]
+    //getElementsByClassName('blueColor');
+    console.log(colorchoice);
+})
