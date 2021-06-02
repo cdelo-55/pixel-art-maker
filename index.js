49245e68dc1212800ce63f2816e35765b70d8c81
Delo <34003@nelnet.net>
2021-06-02 11:37:01 -0500

    pixel art day 2

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const grid = document.querySelector('.grid')
+for (let i = 0; i < 400; i++) {
+    const cell = document.createElement('div');
+    cell.id = i
+    cell.classList.add('cell');
+    grid.appendChild(cell)
+    cell.classList.add
+  }
 
-let cellColor;
 let currentColor;
 let currentCell;
 
 document.getElementById('pallete').addEventListener('click',function(newColor){
   currentColor = newColor.target.id;
-  console.log(currentColor);
+  //console.log(currentColor);
 })
 
-
 document.getElementById('grid').addEventListener('click',function(name){
-  name.target.style.background=`${currentColor}`;
+  if (name.target.classList !== "grid") {
+    name.target.style.background=`${currentColor}`;
+  }
+  //console.log(name.target.classList);
 })
